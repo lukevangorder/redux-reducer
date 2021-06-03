26686b88573df441921dd58d58357052a892b40d
lukevangorder <lukevangorder@gmail.com>
2021-06-03 14:36:57 -0400

    commit tree f7bcc2c7142f8705575b306344f0db2e4836c1d2

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return { friends: state.friends.concat(action.friend) }
+        case 'REMOVE_FRIEND':
+            let stillFriends = []
+            for (const friend of state.friends) {
+                if (friend.id != action.id) {
+                    stillFriends.concat(friend)
+                    return {friends: friend}
+                }
+            }
+            return {friends: stillFriends}
+        default:
+            return state;
+    }
 }
